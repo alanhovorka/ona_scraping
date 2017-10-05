89aa0f92d2f4d30d8f670c00ebee98f7887e9c51
alanhovorka <alanhovorka94@gmail.com>
2017-10-05 14:22:32 -0400

    pulled sites

---
+++ pullsites.py
@@ -0,0 +1,5 @@
+from bs4 import BeautifulSoup
+import os
+
+pages = os.listdir("directory_pages")
+print(pages)
--- scrapePageToLocal.py
+++ scrapePageToLocal.py
@@ -1,9 +1,14 @@
+import json
 import requests
 
 def run(url):
 	r = requests.get(url)
 	name = url.split('/')[-1].split('#')[0]
-	with open('directory_pages/' + name, 'w') as file:
+	with open('directory_pages/' + name, 'w', encoding='utf8') as file:
 		file.write(r.text)
 
-run("https://www.tripadvisor.com/Attractions-g28970-Activities-oa450-Washington_DC_District_of_Columbia.html#ATTRACTION_LIST")
+with open("urlsToScrape.json", 'r') as file:
+	urls = json.loads(file.read())
+	for url in urls:
+		run(url)
+	#print(urls)
