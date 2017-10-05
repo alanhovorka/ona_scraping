4545345924407929d31d982f6c3e7389966c4d1f
alanhovorka <alanhovorka94@gmail.com>
2017-10-05 16:47:31 -0400

    scraping_results

---
+++ process_attract.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import os
+import requests
+import json
+
+print(len(os.listdir("listing_pages")))
+print(len(os.listdir("subdirectories")))
--- pullsites.py
+++ pullsites.py
@@ -3,22 +3,34 @@ import os
 import requests
 import json
 
-def run(url):
+def download(url, dest):
 	r = requests.get(url)
 	name = url.split('/')[-1].split('#')[0]
-	with open('directory_pages/' + name, 'w', encoding='utf8') as file:
+	with open(dest +'/' + name, 'w', encoding='utf8') as file:
 		file.write(r.text)
 
 def extract_site_url(file):
-	with open("directory_pages/" + file, "r") as htmlfile:
+	with open("subdirectories/" + file, "r", encoding='utf8') as htmlfile:
 		bs=BeautifulSoup(htmlfile.read(), "html.parser")
 		#print(bs)
 		titles=bs.find_all("div",{"class":"listing_title"})
 		print(len(titles))
 		for title in titles:
 			#print(title.find("a").string)
-			print(title.find("a")["href"])
+			href=title.find("a")["href"]
+			check=href[0:12]
+			if check == "/Attraction_":
+				download("https://www.tripadvisor.com" + href, "listing_pages")
+			elif check == "/Attractions":
+				download("https://www.tripadvisor.com" + href, "subdirectories")
 
-pages = os.listdir("directory_pages")
-extract_site_url(pages[0])
+#[11]/Attractions-g28970-Activities-c61-t167-Washington_DC_District_of_Columbia.html
+#[17]/Attraction_Review-g28970-d130112-Reviews-Corcoran_Gallery_of_Art-Washington_DC_District_of_Columbia.html
+
+
+
+pages = os.listdir("subdirectories")
+#extract_site_url(pages[0])
+for page in pages:
+	extract_site_url(page)
 #print(pages)
