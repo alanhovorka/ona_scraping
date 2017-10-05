c9767650835bf3c1ce91867d7972004fa9e4c759
alanhovorka <alanhovorka94@gmail.com>
2017-10-05 14:52:32 -0400

    scraper building

--- pullsites.py
+++ pullsites.py
@@ -1,5 +1,24 @@
 from bs4 import BeautifulSoup
 import os
+import requests
+import json
+
+def run(url):
+	r = requests.get(url)
+	name = url.split('/')[-1].split('#')[0]
+	with open('directory_pages/' + name, 'w', encoding='utf8') as file:
+		file.write(r.text)
+
+def extract_site_url(file):
+	with open("directory_pages/" + file, "r") as htmlfile:
+		bs=BeautifulSoup(htmlfile.read(), "html.parser")
+		#print(bs)
+		titles=bs.find_all("div",{"class":"listing_title"})
+		print(len(titles))
+		for title in titles:
+			#print(title.find("a").string)
+			print(title.find("a")["href"])
 
 pages = os.listdir("directory_pages")
-print(pages)
+extract_site_url(pages[0])
+#print(pages)
